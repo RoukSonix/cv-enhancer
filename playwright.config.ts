b6aa852e7103a3f6b16724919d4694818d149a6b
Andrey Lazarev <a.lazarev@creatio.com>
2026-03-12 10:40:15 +0000

    chore: change app port from 3000 to 8601

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 60_000,
   retries: 1,
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8601",
     headless: true,
   },
   projects: [
